abeaf473eaa70f269d1ad62834ac9d421e0dd040
PauloSandaAtlas <paulo.sanda@atlastechnol.com>
2022-03-01 18:48:41 -0300

    Swagger 1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,25 +8,21 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 /**
-     * @OA\Info(
-     *      version="1.0.0",
-     *      title="Challenge SpaceNews",
-     *      description="Just a simple challenge",
-     *      @OA\Contact(
-     *          email="paulosanda@gmail.com",
-     *      )
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST,
-     *      description="SpaceNews"
-     * )
-     *
-     * @OA\Tag(
-     *     name="Gateways (Public)",
-     *     description="This can be done by all requests",
-     * )
-     */
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Challenge SpaceNews 🏅",
+ *      description="Just a simple challenge",
+ *      @OA\Contact(
+ *          email="paulosanda@gmail.com",
+ *      )
+ * )
+ * @OA\Get(
+ *      path="/",
+ *      @OA\Response(response="200", 
+ *      description="Display"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
